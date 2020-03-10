d56ab78ad2d76affac9126c6215adcdc1ca95b18
Steve Sha <steve.sha@tvnz.co.nz>
2020-03-10 16:07:59 +1300

    Remove the blank lines

--- src/main/java/chapter1/Exercise15.java
+++ src/main/java/chapter1/Exercise15.java
@@ -1,18 +1,40 @@
 package chapter1;
 
+import java.util.ArrayList;
+
 /**
  * chapter1.Exercise15
  *
  * @author Quanyu Sha
  * @date 05/03/2020
  */
-
 public class Exercise15 {
 
     /* Question
-    Write a program that stores Pascal's triangle up to a given n in an ArrayList<ArrayList<Integer>>
+    Write a program that stores Pascal's triangle up to a given n in an ArrayList<ArrayList<Integer>>.
+
+    Pascal's triangle:
+    1
+    1 1
+    1 2 1
+    1 3 3 1
+    1 4 6 4 1
+    ...
     */
 
-    public static void main(String[] args) {
+    public static ArrayList<ArrayList<Integer>> pascalTriangleInArrayList(int n) {
+        ArrayList<ArrayList<Integer>> pascalsTriangle = new ArrayList<ArrayList<Integer>>();
+
+        // using math formula
+        for (int line = 1; line <= n; line++) {
+            int c = 1;
+            ArrayList<Integer> lineInts = new ArrayList<Integer>();
+            for (int i = 1; i <= line; i++) {
+                lineInts.add(c);
+                c = c * (line - i) / i;
+            }
+            pascalsTriangle.add(lineInts);
+        }
+        return pascalsTriangle;
     }
 }
